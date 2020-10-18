d904fe46eb1ce8c6606e252e80bcae4530170e40
Dhruvin <shah.dhruvin90@gmail.com>
2020-10-18 12:20:23 +0530

    Version 1.6.5
    Version 1.6.5

--- includes/settings/class-alg-wc-custom-payment-gateways-settings-advanced.php
+++ includes/settings/class-alg-wc-custom-payment-gateways-settings-advanced.php
@@ -5,61 +5,68 @@
  * @version 1.4.0
  * @since   1.4.0
  * @author  Tyche Softwares
+ * @package cpgw
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
 if ( ! class_exists( 'Alg_WC_Custom_Payment_Gateways_Settings_Advanced' ) ) :
 
-class Alg_WC_Custom_Payment_Gateways_Settings_Advanced extends Alg_WC_Custom_Payment_Gateways_Settings_Section {
-
 	/**
-	 * Constructor.
-	 *
-	 * @version 1.4.0
-	 * @since   1.4.0
+	 * Advanced Settings class.
 	 */
-	function __construct() {
-		$this->id   = 'advanced';
-		$this->desc = __( 'Advanced', 'custom-payment-gateways-woocommerce' );
-		parent::__construct();
-	}
+	class Alg_WC_Custom_Payment_Gateways_Settings_Advanced extends Alg_WC_Custom_Payment_Gateways_Settings_Section {
 
-	/**
-	 * get_settings.
-	 *
-	 * @version 1.4.0
-	 * @since   1.4.0
-	 */
-	function get_settings() {
-		$settings = array(
-			array(
-				'title'    => __( 'Advanced Options', 'custom-payment-gateways-woocommerce' ),
-				'type'     => 'title',
-				'id'       => 'alg_wc_cpg_advanced_options',
-			),
-			array(
-				'title'    => __( 'Shipping methods', 'custom-payment-gateways-woocommerce' ),
-				'desc_tip' => __( 'Used in "Enable for shipping methods" custom payment gateway\'s option.', 'custom-payment-gateways-woocommerce' ),
-				'type'     => 'select',
-				'class'    => 'chosen_select',
-				'id'       => 'alg_wc_cpg_load_shipping_method_instances',
-				'default'  => 'yes',
-				'options'  => array(
-					'yes'     => __( 'Load shipping methods and instances', 'custom-payment-gateways-woocommerce' ),
-					'no'      => __( 'Load shipping methods only', 'custom-payment-gateways-woocommerce' ),
-					'disable' => __( 'Do not load', 'custom-payment-gateways-woocommerce' ),
+		/**
+		 * Constructor.
+		 *
+		 * @version 1.4.0
+		 * @since   1.4.0
+		 */
+		public function __construct() {
+			$this->id   = 'advanced';
+			$this->desc = __( 'Advanced', 'custom-payment-gateways-woocommerce' );
+			parent::__construct();
+		}
+
+		/**
+		 * Get settings.
+		 *
+		 * @return array Settings Array.
+		 * @version 1.4.0
+		 * @since   1.4.0
+		 */
+		public function get_settings() {
+			$settings = array(
+				array(
+					'title' => __( 'Advanced Options', 'custom-payment-gateways-woocommerce' ),
+					'type'  => 'title',
+					'id'    => 'alg_wc_cpg_advanced_options',
 				),
-			),
-			array(
-				'type'     => 'sectionend',
-				'id'       => 'alg_wc_cpg_advanced_options',
-			),
-		);
-		return $settings;
-	}
+				array(
+					'title'    => __( 'Shipping methods', 'custom-payment-gateways-woocommerce' ),
+					'desc_tip' => __( 'Used in "Enable for shipping methods" custom payment gateway\'s option.', 'custom-payment-gateways-woocommerce' ),
+					'type'     => 'select',
+					'class'    => 'chosen_select',
+					'id'       => 'alg_wc_cpg_load_shipping_method_instances',
+					'default'  => 'yes',
+					'options'  => array(
+						'yes'     => __( 'Load shipping methods and instances', 'custom-payment-gateways-woocommerce' ),
+						'no'      => __( 'Load shipping methods only', 'custom-payment-gateways-woocommerce' ),
+						'disable' => __( 'Do not load', 'custom-payment-gateways-woocommerce' ),
+					),
+				),
+				array(
+					'type' => 'sectionend',
+					'id'   => 'alg_wc_cpg_advanced_options',
+				),
+			);
+			return $settings;
+		}
 
-}
+	}
 
 endif;
 
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: tychesoftwares, ashokrane, dhruvin
 Tags: woocommerce, custom payment gateways, custom payment gateway, payment gateways, payment gateway
 Requires at least: 4.4
-Tested up to: 5.3
-Stable tag: 1.6.4
+Tested up to: 5.5
+Stable tag: 1.6.5
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -49,6 +49,10 @@ With [Pro version](https://wpfactory.com/item/custom-payment-gateways-woocommerc
 3. Start by visiting plugin settings at "WooCommerce > Settings > Custom Payment Gateways".
 
 == Changelog ==
+
+= 1.6.5 - 18/10/2020
+* Code refactoring as per WPCS standards
+* WC tested up to: 4.6
 
 = 1.6.4 - 12/03/2020 =
 * WC tested up to: 4.0
